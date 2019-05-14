427a04ae6c974c8c43725606d8ea5a7e34b116b7
krob030 <krob030@gmail.com>
2019-05-14 01:45:30 -0500

    hwk

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sqlalchemy
+import datetime
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -8,8 +9,8 @@ from flask import Flask, jsonify
 engine = create_engine("sqlite:///hawaii.sqlite", echo=False)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-Measurement= Base.classes.measurements
-Station= Base.classes.stations
+Measurement= Base.classes.measurement
+Station= Base.classes.station
 session= Session(engine)
 
 app= Flask(__name__)
@@ -20,16 +21,17 @@ def home():
         f"Home page"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/measurements<br/>"
-        f"/api/v1.0/stations"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitaion():
-    result= session.query(Measurement.date, Measurement.prcp).all()
-"""Dictionary"""
-    precip = []
-    for date, prcp in result:
+    results= session.query(Measurement.date, Measurement.prcp).all()
+    all_precip= []
+    for date, prcp in results:
         precip_dict= {}
         precip_dict["date"]= date
         precip_dict["prcp"]= prcp
@@ -38,21 +40,29 @@ def precipitaion():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    results = session.query(Station.station, Satation.name).all()
-    return jsonify(results)
+    results = session.query(Station.station, Station.name).all()
+    station_list= list(np.ravel(results))
+    return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    results= session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).all()
-    return jsonify(results)
+    results= session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "08-23-2017").all()
+    date_tobs= list(np.ravel(results))
+    return jsonify(date_tobs)
 
-@app.route("/api/v1.0/<start>")
-def tobs():
-    return jsonify(results)
+@app.route("/api/v1.0/start")
+def start(start_date):
+    results= session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).all()
+    calc_start= list(np.ravel(results))
+    return jsonify(calc_start)
+    
+@app.route("/api/v1.0/start/end")
+def start_end(start_date, end_date):
+    results= session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    calc_startend= list(np.ravel(results))
+    return jsonify(calc_startend)
 
-@app.route("api/v1.0/<start>/<end>")
-def tobs():
-    return jsonify(results)
-
-if__name__== "__main__":
+if __name__== "__main__":
     app.run(debug=True)
