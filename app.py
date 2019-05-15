dae9c6e41c603b96bdeb4224ffd0d6a8c0a2f99f
krob030 <krob030@gmail.com>
2019-05-14 21:21:37 -0500

    last

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import numpy as np
 import sqlalchemy
-import datetime
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-engine = create_engine("sqlite:///hawaii.sqlite", echo=False)
+engine = create_engine("sqlite:///hawaii.sqlite?check_same_thread=False")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 Measurement= Base.classes.measurement
@@ -23,7 +23,6 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
         f"/api/v1.0/start/end"
     )
 
@@ -40,27 +39,21 @@ def precipitaion():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    results = session.query(Station.station, Station.name).all()
+    results = session.query(Station.station).all()
     station_list= list(np.ravel(results))
     return jsonify(station_list)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    results= session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= "08-23-2017").all()
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results= session.query(Measurement.tobs).filter(Measurement.date >= prev_year).all()
     date_tobs= list(np.ravel(results))
     return jsonify(date_tobs)
 
-@app.route("/api/v1.0/start")
-def start(start_date):
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start=None, end=None):
     results= session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).all()
-    calc_start= list(np.ravel(results))
-    return jsonify(calc_start)
-    
-@app.route("/api/v1.0/start/end")
-def start_end(start_date, end_date):
-    results= session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
     calc_startend= list(np.ravel(results))
     return jsonify(calc_startend)
 
